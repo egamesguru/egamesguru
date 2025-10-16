cc6566a7f5ab7dcb4f876d6d170a068173cae7b3
Erich <erich@localhost>
2025-10-16 14:19:48 +0200

    Replace mobile & desktop layouts with responsive layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,48 +83,77 @@ const NavBarLinkSidebar = ({
   );
 };
 
-const MobileLayout = ({ children }: { children: React.ReactNode }) => {
+// const MobileLayout = ({ children }: { children: React.ReactNode }) => {
+//   return (
+//     <div className="relative flex flex-col h-screen md:hidden">
+//       <header className="py-3 px-4 flex justify-between">
+//         <Link href="/" color="foreground" className="text-2xl font-bold">
+//           eGamesGuru
+//         </Link>
+//
+//         <div className="h-16 flex items-center">
+//           <MobileSignInButton />
+//         </div>
+//       </header>
+//
+//       <div className="flex-grow overflow-auto px-4 py-2">{children}</div>
+//
+//       <nav className="flex px-2 justify-evenly w-full">
+//         <NavBarLink href="/" label="Newsfeed" active>
+//           <Icons.NewspaperIcon />
+//         </NavBarLink>
+//         <NavBarLink href="/contendership" label="Contendership">
+//           <Icons.NumberedListIcon />
+//         </NavBarLink>
+//         {/* <NavBarLink href="/pinnwand" label="Pinnwand"> */}
+//         {/*   <Icons.VideoCameraIcon /> */}
+//         {/* </NavBarLink> */}
+//         <NavBarLink href="/shop" label="Shop">
+//           <Icons.BuildingStorefrontIcon />
+//         </NavBarLink>
+//         <NavBarLink href="/agency" label="Agency">
+//           <Icons.PresentationChartLineIcon />
+//         </NavBarLink>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// const DesktopLayout = ({ children }: { children: React.ReactNode }) => {
+//   return (
+//     <div className="hidden md:flex h-screen">
+//       <div className="p-6 pt-10 w-md flex flex-col min-w-0">
+//         <header>
+//           <Link href="/" color="foreground" className="text-3xl font-bold">
+//             eGamesGuru
+//           </Link>
+//         </header>
+//         <div className="flex flex-col justify-between items-stretch max-w-full grow">
+//           <nav className="mt-8 flex flex-col gap-4">
+//             <NavBarLinkSidebar href="/">Newsfeed</NavBarLinkSidebar>
+//             <NavBarLinkSidebar href="/contendership">
+//               Contendership
+//             </NavBarLinkSidebar>
+//             <NavBarLinkSidebar href="/shop">Shop</NavBarLinkSidebar>
+//             <NavBarLinkSidebar href="/agency">Agency</NavBarLinkSidebar>
+//           </nav>
+//
+//           <div className="max-w-full">
+//             <SignInButton />
+//           </div>
+//         </div>
+//       </div>
+//       <div className="pt-10 w-full overflow-y-hidden max-h-full">
+//         {children}
+//       </div>
+//     </div>
+//   );
+// };
+
+const SidebarDesktop = () => {
   return (
-    <div className="relative flex flex-col h-screen md:hidden">
-      <header className="py-3 px-4 flex justify-between">
-        <Link href="/" color="foreground" className="text-2xl font-bold">
-          eGamesGuru
-        </Link>
-
-        {/* <Button isIconOnly variant="light"> */}
-        {/*   <Icons.Bars3Icon className="w-8" /> */}
-        {/* </Button> */}
-
-        <div className="h-16 flex items-center">
-          <MobileSignInButton />
-        </div>
-      </header>
-      <div className="flex-grow overflow-auto px-4 py-2">{children}</div>
-      <nav className="flex px-2 justify-evenly w-full">
-        <NavBarLink href="/" label="Newsfeed" active>
-          <Icons.NewspaperIcon />
-        </NavBarLink>
-        <NavBarLink href="/contendership" label="Contendership">
-          <Icons.NumberedListIcon />
-        </NavBarLink>
-        {/* <NavBarLink href="/pinnwand" label="Pinnwand"> */}
-        {/*   <Icons.VideoCameraIcon /> */}
-        {/* </NavBarLink> */}
-        <NavBarLink href="/shop" label="Shop">
-          <Icons.BuildingStorefrontIcon />
-        </NavBarLink>
-        <NavBarLink href="/agency" label="Agency">
-          <Icons.PresentationChartLineIcon />
-        </NavBarLink>
-      </nav>
-    </div>
-  );
-};
-
-const DesktopLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="hidden md:flex h-screen">
-      <div className="p-6 pt-10 w-md flex flex-col min-w-0">
+    <>
+      <div className="p-6 pt-10 w-xs flex-col min-w-0 hidden md:flex">
         <header>
           <Link href="/" color="foreground" className="text-3xl font-bold">
             eGamesGuru
@@ -145,10 +174,65 @@ const DesktopLayout = ({ children }: { children: React.ReactNode }) => {
           </div>
         </div>
       </div>
-      <div className="pt-10 w-full overflow-y-hidden max-h-screen">
-        {children}
+    </>
+  );
+};
+
+const TopbarMobile = () => {
+  return (
+    <>
+      <header className="py-3 px-4 flex justify-between md:hidden">
+        <Link href="/" color="foreground" className="text-2xl font-bold">
+          eGamesGuru
+        </Link>
+
+        <div className="h-16 flex items-center">
+          <MobileSignInButton />
+        </div>
+      </header>
+    </>
+  );
+};
+
+const BottombarMobile = () => {
+  return (
+    <>
+      <nav className="flex px-2 justify-evenly w-full md:hidden">
+        <NavBarLink href="/" label="Newsfeed" active>
+          <Icons.NewspaperIcon />
+        </NavBarLink>
+        <NavBarLink href="/contendership" label="Contendership">
+          <Icons.NumberedListIcon />
+        </NavBarLink>
+        {/* <NavBarLink href="/pinnwand" label="Pinnwand"> */}
+        {/*   <Icons.VideoCameraIcon /> */}
+        {/* </NavBarLink> */}
+        <NavBarLink href="/shop" label="Shop">
+          <Icons.BuildingStorefrontIcon />
+        </NavBarLink>
+        <NavBarLink href="/agency" label="Agency">
+          <Icons.PresentationChartLineIcon />
+        </NavBarLink>
+      </nav>
+    </>
+  );
+};
+
+const ResponsiveLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <div className="flex flex-col md:flex-row h-screen overflow-y-hidden">
+        <SidebarDesktop />
+
+        <TopbarMobile />
+
+        <div className="grow mt-10 px-4 md:px-0 overflow-y-auto md:overflow-y-hidden">
+          {children}
+        </div>
+
+        <BottombarMobile />
       </div>
-    </div>
+    </>
   );
 };
 
@@ -167,8 +251,9 @@ export default function RootLayout({
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <MobileLayout>{children}</MobileLayout>
-          <DesktopLayout>{children}</DesktopLayout>
+          <ResponsiveLayout>{children}</ResponsiveLayout>
+          {/* <MobileLayout>{children}</MobileLayout> */}
+          {/* <DesktopLayout>{children}</DesktopLayout> */}
         </Providers>
       </body>
     </html>
