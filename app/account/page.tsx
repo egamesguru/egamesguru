5d972452e41f25c6bc7c02d8515ebbeb2d51b08a
Erich <erich@localhost>
2025-10-16 17:38:54 +0200

    remove unused import

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 import { Avatar } from "@heroui/avatar";
 import { Button } from "@heroui/button";
-import { avatar } from "@heroui/theme";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ArrowRightStartOnRectangleIcon } from "@heroicons/react/24/solid";
