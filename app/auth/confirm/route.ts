31d59b12b85b753c3fb88ddb24278f217abee594
Erich <erich@localhost>
2025-10-18 21:16:09 +0200

    Fix /auth/confirm redirect in prod

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,19 +1,13 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
-  const next = "/account";
-
-  const redirectTo = request.nextUrl.clone();
-  console.log("redirectTo", redirectTo);
-
-  redirectTo.pathname = next;
-  redirectTo.searchParams.delete("token_hash");
-  redirectTo.searchParams.delete("type");
+  const next = searchParams.get("next") ?? "/account";
 
   if (token_hash && type) {
     const supabase = await createClient();
@@ -23,6 +17,7 @@ export async function GET(request: NextRequest) {
       token_hash,
     });
 
+    console.log("/auth/confirm");
     console.log("user data", data);
     console.log("user metadata", data?.user?.user_metadata);
     const fullname = (data?.user?.user_metadata.fullname as string) ?? "";
@@ -34,12 +29,10 @@ export async function GET(request: NextRequest) {
         .from("profiles")
         .upsert({ id: uid!, fullname });
       if (!error) {
-        redirectTo.searchParams.delete("next");
-        return NextResponse.redirect(redirectTo);
+        redirect(next);
       }
     }
   }
 
-  redirectTo.pathname = "/error";
-  return NextResponse.redirect(redirectTo);
+  return redirect("/error");
 }
